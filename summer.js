0488a117591e24cfd99b5b2fb27d80c53c07533d
Kendrick Taylor <kendrick@circuitsix.com>
2017-03-25 16:41:01 -0700

    inital commit. summer works, requires leading 0 on floats. ex: 0.1

---
+++ summer.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var _ = require('dry-underscore');
+
+function extract_line(line){
+
+    line = _.replace(line, ",", "");
+
+    // var regex = "(?=.)([+-]?([0-9]*)(\.([0-9]+))?)";
+    // var regex = "-?(([0-9]+.[0-9]*)|([0-9]*.[0-9]+)|([0-9]+))"
+    // var regex = "-?([0-9]+\.[0-9]*|[0-9]+|\.[0-9]+)"
+    var regex = "-?([0-9]+(:?[.][0-9]*)?)"
+
+    var matcher = _.regex(regex, "g");
+
+    var numbers = line.match(matcher);
+
+    _.p("extract_line.matches: ", numbers);
+
+    numbers = _.map(numbers, function(n){ return(_.n(n)); });
+
+    // _.p("extract_line.numbers: ", numbers);
+
+    return(numbers);
+}
+
+function extract(text){
+    var lines = text.split('\n');
+
+    return(_.flatten(_.map(lines, extract_line)));
+}
+
+function sum(text){ return _.round(_.sum(extract(text)), 2); }
+
+exports.extract = extract;
+exports.sum = sum;
---
+++ summer.test.js
@@ -0,0 +1,50 @@
+
+var _ = require('dry-underscore');
+
+var eq = _.test.eq;
+
+var summer = require('./summer.js');
+
+suite("summer");
+
+function number_array(a){ return(_.map(a, function(n){ return(_.n(n)); })); }
+
+test('integer extract', function(){
+
+    var expected = ["1", "-2", "3", "-4"];
+    var text = expected.join("\n");
+
+    expected = number_array(expected);
+
+    var actual = summer.extract(text);
+
+    eq(actual, expected);
+});
+
+
+test('float extract', function(){
+
+    var text =     ["0.1", "-0.1", "0.1", "1.123", "-2.345", "3.456", "-4.567"].join("\n");
+    var expected = [0.1, -0.1, 0.1, 1.123, -2.345, 3.456, -4.567];
+
+    var actual = summer.extract(text);
+
+    eq(actual, expected);
+});
+
+test('complex extract and sum', function(){
+
+    var line_one = "blah 1.123 some text 1 0.1 0.1 -0.1";
+    var line_one_expected = [1.123, 1, 0.1, 0.1, -0.1];
+
+    var expected = _.concat(line_one_expected, [-2.345, -2.345, 3.456, 5, 6, 700, 8, -4.567]);
+    var text = [line_one, "(-2.345) some more text (-2.345)", "3.456 more text 5 6 700 8", "more text -4.567"].join("\n");
+
+    var actual = summer.extract(text);
+
+    var sum = summer.sum(text);
+    eq(sum, _.sum(expected));
+
+    eq(actual, expected);
+
+});
